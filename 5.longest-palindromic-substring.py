f8129e2dd6fe8c6a789c4deb8f26ec8a51e97173
RyanYin04 <yry@Ryans-Macbook.local>
2019-11-25 19:48:19 -0800

    Problem 5 solved

--- 5.longest-palindromic-substring.py
+++ 5.longest-palindromic-substring.py
@@ -7,6 +7,21 @@
 # @lc code=start
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        
-# @lc code=end
+        maxS = ''
+        maxL = 0
+        for i in range(len(s)):
+            ## Treat each element as the middle of a palindrome string.
+            ## And there is two conditions:
+            ### One is the palidrome string to be returned has an odd lenth, the other is that the length is even.
+            maxS = max(self.helper(s, i, i), self.helper(s, i, i+1), maxS, key = len)
+            maxL = len(s)
+        return maxS
+    
+    def helper(self, s, lm, rm):
+        ## Hleper function returns the palindrome subsrting by set the left middle point and right middle point as well as the string to be searched.
+        while lm >= 0 and rm <len(s) and s[lm] == s[rm] :
+            lm -= 1
+            rm += 1
+        return s[lm+1: rm]
 
+# @lc code=end
