784bb07ab1d1980eb261082072beb19097ad7382
RyanYin04 <yry@Ryans-Macbook.local>
2019-12-21 21:33:45 -0800

    #38

--- 38.count-and-say.py
+++ 38.count-and-say.py
@@ -7,6 +7,24 @@
 # @lc code=start
 class Solution:
     def countAndSay(self, n: int) -> str:
+        if n == 1:
+            return '1'
+        else:
+            back = self.countAndSay(n - 1)
+            result = ''
+            i = 0
+            while i < len(back):
+                count = 1
+                currVal = back[i]
+                while i < len(back) - 1 and back[i] == back[i +1]:
+                    count += 1
+                    i += 1
+                i += 1
+                result += str(count) + currVal
+        return result
+                
         
 # @lc code=end
 
+s = Solution()
+s.countAndSay(25)
